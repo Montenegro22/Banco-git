f714c9e3b28389d8adc10857c4b195756339db73
Natalia <montenegro.natismontenegro@hotmail.com>
2020-08-25 13:03:12 -0500

    agregar comentario

--- src/banco1/Cuenta_corriente.java
+++ src/banco1/Cuenta_corriente.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package banco1;
 
+package banco1;
 /**
- *
  * @author Natalia Montenegro Q
  */
 public class Cuenta_corriente {
-    
+    //Hola papa
 }
